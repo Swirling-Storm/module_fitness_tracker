1e82c6a4c8cfb8ae8a5776c5f42ea16fb0306cfe
Егор Кабитов <kabitov.egor@ya.ru>
2023-02-11 20:52:40 +0300

    After first review

--- homework.py
+++ homework.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass, asdict
-from typing import Dict
 
 
 @dataclass
@@ -131,7 +130,7 @@ class Swimming(Training):
 
 def read_package(workout_type: str, data: list[int]) -> Training:
     """Прочитать данные полученные от датчиков."""
-    types: Dict[str, Training] = {
+    types = {
         'SWM': Swimming,
         'RUN': Running,
         'WLK': SportsWalking
